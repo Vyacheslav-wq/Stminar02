62b272861845e31a666619c369899a713215ed5b
Vyacheslav-wq <124440610+Vyacheslav-wq@users.noreply.github.com>
2023-04-08 08:21:27 +0300

    Удаление средней цифры в трехзначном числе

---
+++ Seen11_MiddleDelete/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу, которая выводит случайное трехзначное число
+// и удаляет вторую цифру этого числа.
+// 456 -> 46
+//918 = 9 * 100 + 1 * 10 + 8
+//98 = 9 * 10 + 8 
+Console.Clear();
+int num = new Random().Next(100, 1000);
+int result = (num / 100) * 10 + num%10;
+Console.WriteLine($"{num} --> {result}");
+
+//Второй метод решения этой задачи.
+// Console.Clear();
+// int num = new Random().Next(100, 1000);
+// Console.WriteLine($"{num} --> {num/100}{num%10}");
